e78e3a032cf21477cb02c39f3997af672d758164
Talyssonbs <talyssonbs@gmail.com>
2022-05-10 18:30:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/* Apod GET https://api.nasa.gov/planetary/apod */
+/* $ (“p”). click () */
+$("#pesquisar").click(function () {
+    const data = $("data").val()
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=dwlJTi9HjQnBiFakRKDldBivQUBi2gQax7v0929X&date=${data}`
+        success: function (integrar)
+    })
+})
