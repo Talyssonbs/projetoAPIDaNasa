18c5742c9125a2f598d41e4b208c284b47a6d46a
Talyssonbs <talyssonbs@gmail.com>
2022-05-11 17:59:47 -0300

    Função de exibir videos inserida

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ $("#pesquisar").click(function () {
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=mAiNZGEMKAUK6JGFaiUXHQsvt37EAgf4iH13zPnQ&date=${dia}`,
         success: function (integrar) {
-            console.log(integrar);
-            /*  $("#titulo").text(integrar.title); */
+            console.log(integrar)
+            $("#titulo").text(integrar.title)
+            $("#descri").text(integrar.explanation)
+            if (integrar.media_type === "imagem") {
+                $("#imagem").attr("src", integrar.url);
+                $("#imagen").show();
+            } else {
+                $("#video").attr("src", integrar.url);
+                $("#video").show();
+            }
         },
         error: function (erro) {
             console.log(erro);
