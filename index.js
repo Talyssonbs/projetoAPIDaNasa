d97d71dd2e516beb51447e51675bca9e9ce2d5b1
Talyssonbs <talyssonbs@gmail.com>
2022-05-10 22:04:06 -0300

    Adição de comentários, HTML e JavaScript

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 $("#pesquisar").click(function () {
     const data = $("data").val()
     $.ajax({
-        url: `https://api.nasa.gov/planetary/apod?api_key=dwlJTi9HjQnBiFakRKDldBivQUBi2gQax7v0929X&date=${data}`
-        success: function (integrar)
+        url: `https://api.nasa.gov/planetary/apod?api_key=dwlJTi9HjQnBiFakRKDldBivQUBi2gQax7v0929X&date=${data}`,
+        success: function (integrar) {
+            $
+        }
     })
 })
