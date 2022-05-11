60071fcdab818174330849a875a557fc19e4845c
Talyssonbs <talyssonbs@gmail.com>
2022-05-11 17:09:35 -0300

    Correção da requisição (Requisição funcionando)

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 /* Apod GET https://api.nasa.gov/planetary/apod */
 /* $ (“p”). click () */
 $("#pesquisar").click(function () {
-    const data = $("data").val()
+    const dia = $("#data").val();
     $.ajax({
-        url: `https://api.nasa.gov/planetary/apod?api_key=dwlJTi9HjQnBiFakRKDldBivQUBi2gQax7v0929X&date=${data}`,
+        url: `https://api.nasa.gov/planetary/apod?api_key=mAiNZGEMKAUK6JGFaiUXHQsvt37EAgf4iH13zPnQ&date=${dia}`,
         success: function (integrar) {
-            $
-        }
-    })
-})
+            console.log(integrar);
+            /*  $("#titulo").text(integrar.title); */
+        },
+        error: function (erro) {
+            console.log(erro);
+        },
+    });
+});
